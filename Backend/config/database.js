52096a9cebbd28a1241c2d5827c2e24c8b2bc778
Mohd Samir <84841504+Mohdsamiransari@users.noreply.github.com>
2023-05-26 21:22:10 +0530

    Delete Backend directory

--- Backend/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDatabase = () => {
-  mongoose.connect(process.env.DB_LOCAL_URI, {}).then((connect) => {
-    console.log(`Connect to database ${connect.connection.host}`);
-  });
-};
-
-module.exports = connectDatabase;
